2963f7073283b87290ddee17e12e4e5fd5c31059
William Le <will@place.technology>
2023-09-07 00:56:48 -0700

    feat: config for kubernetes deployments

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,27 +5,8 @@ set -eu
 # PlaceOS nginx entrypoint
 
 nginx_config="/etc/nginx"
-certs_path="${nginx_config}/ssl/default-domain"
 
-# Generate self-signed SSL certificates if none present
-if [ ! -d "${certs_path}" ]; then
-  mkdir -p "${certs_path}"
-
-  openssl req -newkey rsa:2048 -nodes -days 365 -x509 \
-    -subj "/C=AU/ST=NSW/L=Sydney/O=PlaceOS/CN=${PLACE_DOMAIN}" \
-    -keyout "${certs_path}/privkey.pem" -out "${certs_path}/fullchain.pem"
-
-  openssl dhparam -out "${nginx_config}/ssl/dhparam.pem" 1024
-fi
-
-# Extract the port to use when upgrading connection to HTTPS if it has not been
-# explicitly set. This port may differ from the local HTTPS when remapped within
-# different deployment environments.
-if [ -z ${HTTPS_REDIRECT_PORT+x} ]; then
-  export HTTPS_REDIRECT_PORT=${PLACE_DOMAIN#*:}
-fi
-
-envsubst '$SECRET_KEY_BASE,$HTTPS_REDIRECT_PORT' < ${nginx_config}/nginx.conf.template > ${nginx_config}/nginx.conf
+envsubst '$SECRET_KEY_BASE' < ${nginx_config}/nginx.conf.template > ${nginx_config}/nginx.conf
 
 # Start OpenResty
 /usr/local/openresty/bin/openresty -c "${nginx_config}/nginx.conf"
