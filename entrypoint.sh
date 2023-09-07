cc0318443e547e0e34aed00e49cd54fb3c712807
William Le <will@place.technology>
2023-09-07 01:51:55 -0700

    fix(template): template and conf file must be in different directories as k8s configMap volume mount will always be ReadOnly

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ set -eu
 
 nginx_config="/etc/nginx"
 
-envsubst '$SECRET_KEY_BASE' < ${nginx_config}/nginx.conf.template > ${nginx_config}/nginx.conf
+envsubst '$SECRET_KEY_BASE' < ${nginx_config}/templates/nginx.conf.template > ${nginx_config}/nginx.conf
 
 # Start OpenResty
 /usr/local/openresty/bin/openresty -c "${nginx_config}/nginx.conf"
